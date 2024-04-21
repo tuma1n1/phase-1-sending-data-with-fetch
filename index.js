2d5e33d5490126c0100ebffee968584115708a6b
Tumaini <tumainikebaya.234@gmail.com>
2024-04-21 21:47:08 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email){
+    const config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ name, email })
+    };
+    return fetch('http://localhost:3000/users', config)
+     .then(response => response.json()) 
+     .then(data => {
+      document.body.innerHTML += 
+      `<p>New ID: ${data.id}</p>`;
+    })
+     .catch(error => {
+      document.body.innerHTML += 
+      `<p>Error: ${error.message}</p>`;
+    });
+};
